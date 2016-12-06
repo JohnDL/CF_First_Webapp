97225dcebaa1413365d846c5150d156533233d7b
John Mannconfig --global user.email jmann25@gmail.com <primary@couragefreedoms-MacBook-Pro.local>
2016-12-06 14:08:20 -0700

    4.3 changes to get products page correct

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203022919) do
+ActiveRecord::Schema.define(version: 20161206193808) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20161203022919) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.string   "size"
+    t.decimal  "price"
   end
 
 end
