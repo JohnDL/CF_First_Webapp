4110397d68d906e61f0b0b4982534ba4978a4e2e
John Mannconfig --global user.email jmann25@gmail.com <primary@courageedomsMBP.lan>
2017-01-21 14:41:17 +0100

    4.3 reset DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206193808) do
+ActiveRecord::Schema.define(version: 20170121132704) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20161206193808) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "color"
     t.string   "size"
     t.decimal  "price"
+    t.string   "color"
   end
 
 end
