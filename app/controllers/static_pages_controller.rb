069b0491906d1db27e79bdcc4f051f99bf3cf349
john mann <jmann25@gmail.com>
2017-02-07 16:11:34 +0000

    5.3 setting up mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,20 @@
 class StaticPagesController < ApplicationController
+  def index
+  end
+  
+  
   def landing_page
     @products = Product.all
   end
 
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'mann.john43@yahoo.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+end
 end
 
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'mann.john43@yahoo.com'
   layout 'mailer'
 end
