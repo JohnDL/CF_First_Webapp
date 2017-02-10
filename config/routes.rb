cdfb1144ff992114d14123d429adb8ad022ab9fe
john mann <jmann25@gmail.com>
2017-02-10 17:37:01 +0000

    5.4 finish styling views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,:path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :users
+  
+  resources :products 
+  
   get 'static_pages/index'
-
-  resources :products
+  
   get 'static_pages/register'
 
   get 'static_pages/login'
