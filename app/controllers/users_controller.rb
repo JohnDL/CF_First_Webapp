48e510c0ca07316846387366ed94eb872b26c85f
john mann <jmann25@gmail.com>
2017-02-12 11:40:45 +0000

    5.5 more testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
+# class UsersController < ApplicationController
+#   before_action :set_user, only: [:show, :edit, :update, :destroy]
+#   before_action :authenticate_user!
+#   load_and_authorize_resource
+  
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
   load_and_authorize_resource
    
 
