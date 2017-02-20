402c77e75219ea840e647f2e23a0cc92e5094d26
john mann <jmann25@gmail.com>
2017-02-20 12:37:50 +0000

    5.7 add comments feature

---
+++ app/models/comment.rb
@@ -0,0 +1,11 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :product
+
+#scope which selects the comments with the highest rating
+  scope :rating_desc, -> {order(rating: :desc)}
+
+  #scope which selects the comments with the lowest rating
+  scope :rating_asc, -> {order(rating: :asc)}
+
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
+
+#custom method for our product to return the comment with the highest rating
+  def highest_rating_comment
+  	comments.rating_desc.first
+  end
+
+   #custom method for our product to return the comment with the lowest rating
+  def lowest_rating_comment
+  	comments.rating_asc.first
+  end
 end
